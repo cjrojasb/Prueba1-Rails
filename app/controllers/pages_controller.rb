3f8980a2c7afbf65cd053799fe30fd3a6af70cb1
Carlos Rojas <carlos.rojas.burgos@gmail.com>
2017-06-21 20:19:38 -0400

    Commit N°4 - Item Superhero finalized

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,25 @@ class PagesController < ApplicationController
   end
 
   def batman_vs_superman
-    
+    @batman_votes = VotosBatman.count
+    @superman_votes = VotosSuperman.count
+  end
+
+  def save_vote_batman
+    if params[:name].present? && params[:email].present? 
+      VotosBatman.create(name:params[:name], email:params[:email])
+      redirect_to pages_batman_vs_superman_path, notice: "Has votado por Batman!"
+    else
+      redirect_to pages_batman_vs_superman_path, alert: "Error, debes completar todos los campos"
+    end
+  end
+
+  def save_vote_superman
+    if params[:name].present? && params[:email].present?
+      VotosSuperman.create(name:params[:name], email:params[:email])
+      redirect_to pages_batman_vs_superman_path, notice: "Has votado por Superman!"
+    else
+      redirect_to pages_batman_vs_superman_path, alert: "Error, debes completar todos los campos"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
 
   get 'pages/superman'
 
-  get 'pages/batman_vs_superman'
+  get 'pages/batman_vs_superman', to:"pages#batman_vs_superman"
+
+  post 'pages/save_vote_batman'
+  
+  post 'pages/save_vote_superman'
 
   # Root
 
